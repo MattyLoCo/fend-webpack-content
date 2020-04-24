958825a3737a5aae416c377a8b4d5c7b99b238db
Matthew Lopez <lopezmatt6891@gmail.com>
2020-04-24 14:53:13 -0600

    Add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
     module: {
         rules: [
             {
@@ -11,5 +12,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
